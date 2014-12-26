14ca071919b328a13967052f36d22f0c3a1f2737
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2014-12-26 22:11:40 +0000

    Adds description in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject geom "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "geom: audio visualizer"
+  :url "http://github.com/lepisma/geom"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
